7f73730c46c8df5afd350b194f82e68d1f61f689
dave-manufor <davemanufor@gmail.com>
2024-11-20 19:58:21 +0300

    .

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER", "localhost:3306");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "0429mysql");
+define("DB_USERNAME", "admin");
+define("DB_PASSWORD", "12345678");
 define("DB_NAME", "drugdb");
 
 $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
